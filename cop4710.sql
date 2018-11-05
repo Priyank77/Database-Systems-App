4b52834734b35d3cf45f0dbd27e6390803fe8ad4
jbelvin15 <jbelvin15@gmail.com>
2018-11-04 23:48:51 -0500

    Add files via upload

---
+++ cop4710.sql
@@ -0,0 +1,469 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2018 at 05:43 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cop4710`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `user_id` int(11) NOT NULL,
+  `org_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `admin_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `age` int(11) NOT NULL,
+  `degree` varchar(40) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `at`
+--
+
+CREATE TABLE `at` (
+  `event_id` int(11) NOT NULL,
+  `owner_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `phone_num` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `time` double NOT NULL,
+  `date` date NOT NULL,
+  `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `category` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `creates`
+--
+
+CREATE TABLE `creates` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `uni_name` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `event_id` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `category` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `time` double NOT NULL,
+  `date` date NOT NULL,
+  `owner_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `phone_num` int(11) NOT NULL,
+  `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `has`
+--
+
+CREATE TABLE `has` (
+  `profile_id` int(11) NOT NULL,
+  `num_students` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `loc` int(11) NOT NULL,
+  `description` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `pics` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `joins`
+--
+
+CREATE TABLE `joins` (
+  `user_id` int(11) NOT NULL,
+  `rso_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loc`
+--
+
+CREATE TABLE `loc` (
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `owns`
+--
+
+CREATE TABLE `owns` (
+  `user_id` int(11) NOT NULL,
+  `org_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `admin_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `age` int(11) NOT NULL,
+  `degree` varchar(40) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `privateevent`
+--
+
+CREATE TABLE `privateevent` (
+  `private_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `profile_id` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `loc` int(11) NOT NULL,
+  `description` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `num_students` int(11) NOT NULL,
+  `pics` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publicevent`
+--
+
+CREATE TABLE `publicevent` (
+  `public_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+
+CREATE TABLE `request` (
+  `user_id` int(11) NOT NULL,
+  `rso_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rso`
+--
+
+CREATE TABLE `rso` (
+  `rso_id` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `category` varchar(40) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rsoevent`
+--
+
+CREATE TABLE `rsoevent` (
+  `rso_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `age` int(11) NOT NULL,
+  `degree` varchar(40) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `superadmin`
+--
+
+CREATE TABLE `superadmin` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `uni_name` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `views`
+--
+
+CREATE TABLE `views` (
+  `public_id` int(11) NOT NULL,
+  `rso_id` int(11) NOT NULL,
+  `private_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `at`
+--
+ALTER TABLE `at`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `creates`
+--
+ALTER TABLE `creates`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `has`
+--
+ALTER TABLE `has`
+  ADD PRIMARY KEY (`profile_id`);
+
+--
+-- Indexes for table `joins`
+--
+ALTER TABLE `joins`
+  ADD PRIMARY KEY (`user_id`,`rso_id`),
+  ADD KEY `rso_id` (`rso_id`);
+
+--
+-- Indexes for table `loc`
+--
+ALTER TABLE `loc`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `owns`
+--
+ALTER TABLE `owns`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `privateevent`
+--
+ALTER TABLE `privateevent`
+  ADD PRIMARY KEY (`private_id`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`profile_id`);
+
+--
+-- Indexes for table `publicevent`
+--
+ALTER TABLE `publicevent`
+  ADD PRIMARY KEY (`public_id`);
+
+--
+-- Indexes for table `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`user_id`,`rso_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `rso_id` (`rso_id`);
+
+--
+-- Indexes for table `rso`
+--
+ALTER TABLE `rso`
+  ADD PRIMARY KEY (`rso_id`);
+
+--
+-- Indexes for table `rsoevent`
+--
+ALTER TABLE `rsoevent`
+  ADD PRIMARY KEY (`rso_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `superadmin`
+--
+ALTER TABLE `superadmin`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `views`
+--
+ALTER TABLE `views`
+  ADD PRIMARY KEY (`public_id`,`rso_id`,`private_id`),
+  ADD KEY `rso_id` (`rso_id`),
+  ADD KEY `private_id` (`private_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `privateevent`
+--
+ALTER TABLE `privateevent`
+  MODIFY `private_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `publicevent`
+--
+ALTER TABLE `publicevent`
+  MODIFY `public_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rso`
+--
+ALTER TABLE `rso`
+  MODIFY `rso_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rsoevent`
+--
+ALTER TABLE `rsoevent`
+  MODIFY `rso_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `superadmin`
+--
+ALTER TABLE `superadmin`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `at`
+--
+ALTER TABLE `at`
+  ADD CONSTRAINT `at_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`);
+
+--
+-- Constraints for table `creates`
+--
+ALTER TABLE `creates`
+  ADD CONSTRAINT `creates_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `superadmin` (`user_id`);
+
+--
+-- Constraints for table `has`
+--
+ALTER TABLE `has`
+  ADD CONSTRAINT `has_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`);
+
+--
+-- Constraints for table `joins`
+--
+ALTER TABLE `joins`
+  ADD CONSTRAINT `joins_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `student` (`user_id`),
+  ADD CONSTRAINT `joins_ibfk_2` FOREIGN KEY (`rso_id`) REFERENCES `rso` (`rso_id`);
+
+--
+-- Constraints for table `owns`
+--
+ALTER TABLE `owns`
+  ADD CONSTRAINT `owns_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `admin` (`user_id`);
+
+--
+-- Constraints for table `request`
+--
+ALTER TABLE `request`
+  ADD CONSTRAINT `request_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `student` (`user_id`),
+  ADD CONSTRAINT `request_ibfk_2` FOREIGN KEY (`rso_id`) REFERENCES `rso` (`rso_id`);
+
+--
+-- Constraints for table `views`
+--
+ALTER TABLE `views`
+  ADD CONSTRAINT `views_ibfk_1` FOREIGN KEY (`public_id`) REFERENCES `publicevent` (`public_id`),
+  ADD CONSTRAINT `views_ibfk_2` FOREIGN KEY (`rso_id`) REFERENCES `rsoevent` (`rso_id`),
+  ADD CONSTRAINT `views_ibfk_3` FOREIGN KEY (`private_id`) REFERENCES `privateevent` (`private_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
